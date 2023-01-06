7dbf2ed65298c81d4d6301340957865308fb70c6
lean <leanndro.garcia@gmail.com>
2023-01-05 23:20:56 -0300

    actualizacion de productos

--- js/main.js
+++ js/main.js
@@ -15,16 +15,33 @@ class Producto {
 }
 
 const producto1 = new Producto("mojave", "Mojave 5.0 R29", "./img/BIN-5.0-29-ROJA.jpg", "MTB", 80000);
-producto1.agregarCatalogo();
-
 const producto2 = new Producto("strada", "Strada 1.0 ", "./img/strada1.0-negra-sm.jpg", "Ruta", 200000);
-producto2.agregarCatalogo();
-
 const producto3 = new Producto("urban", "Urban 1.0", "./img/BIN-URBAN-1.1-GRIS.jpg", "Urbana", 100000);
-producto3.agregarCatalogo();
-
 const producto4 = new Producto("mxr", "Mx Kids", "./img/Scout-Roja-1.jpg", "Kids", 60000);
+const producto5 = new Producto("atc","ATC", "/img/ATC_1_Zoom-2020.jpg","Kids", 65000);
+const producto6 = new Producto("cimaurb", "Cima Urbana", "./img/Cima_URB_1_Zoom-2021.jpg", "Urbana", 110000);
+const producto7 = new Producto("cimaurbdama", "Cima Urbana dama", "./img/Cima_URW_1_Zoom-2021.jpg", "Urbana", 110000);
+const producto8 = new Producto("andeselite", "Andes Elite", "./img/Andes_ELT_1_Zoom-2021.jpg", "MTB", 100000);
+
+
+
+
+producto1.agregarCatalogo();
+producto2.agregarCatalogo();
+producto3.agregarCatalogo();
 producto4.agregarCatalogo();
+producto5.agregarCatalogo();
+producto6.agregarCatalogo();
+producto7.agregarCatalogo();
+producto8.agregarCatalogo();
+
+
+
+
+
+
+
+
 
 const contenedorProductos = document.querySelector("#contenedor-productos");
 const botonesMenu = document.querySelectorAll(".boton-menu");
@@ -38,7 +55,7 @@ function cargarProductos(productosElegidos) {
     const div = document.createElement("div");
     div.classList.add("producto");
     div.innerHTML = `<img class="producto-imagen" src="${producto.imagen}" alt="${producto.nombre}" />
-          <div>
+          <div id="producto-detalle">
             <h3 class="producto-titulo">${producto.nombre}</h3>
             <p class="producto-precio">$${producto.precio}</p>
             <button class="producto-agregar" id="${producto.id}">Agregar</button>
